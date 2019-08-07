8f0f52d7566d22de6fe81bca5c8a5a978894770d
Promise Chukwuenyem <promisepreston@gmail.com>
2019-08-07 11:39:07 +0100

    add response module to controllers concerns

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
 
   # sign up
   def create
-    user = User.new user_params
+    user = User.new(user_params)
     if user.save
       render json: {
           messages: "Signed in successfully",
