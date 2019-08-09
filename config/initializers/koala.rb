6e157f048038f3bd48fdedffdaa5135406369591
Promise Chukwuenyem <promisepreston@gmail.com>
2019-08-09 06:45:03 +0100

    add facebook app config to initializers

---
+++ config/initializers/koala.rb
@@ -0,0 +1,6 @@
+Koala.configure do |config|
+  config.app_id = "713145162489020"
+  config.app_secret = "29e55bd2aada1fd5b83c29d4f52f68b9"
+  # See Koala::Configuration for more options, including details on how to send requests through
+  # your own proxy servers.
+end
